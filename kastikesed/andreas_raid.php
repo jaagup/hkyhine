364a40cfc6b68df50dc3b4a1140ba4ce7c797fe9
Andreas Antti Raid <andreasantti.raid@tigu.hk.tlu.ee>
2015-04-02 17:14:36 +0300

    huehue

--- kastikesed/andreas_raid.php
+++ kastikesed/andreas_raid.php
@@ -8,7 +8,7 @@
 	<form action="naitaja.php">
 		<input type="text" name="eesnimi"><br />
 		<ul>
-			<li><input type="checkbox" name="omadused[]" value="ilus">Ilus</li>
+			<li><input type="checkbox" name="omadused[]" value="ilus">Oh daaaaaayumm!</li>
 			<li><input type="checkbox" name="omadused[]" value="tark">Tark</li>
 			<li><input type="checkbox" name="omadused[]" value="osav">OsavLoom</li>
 			<li><input type="checkbox" name="omadused[]" value="kaunis">Kaunis</li>
