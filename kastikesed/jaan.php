cfa0a0e6f3713a015db597d0275a3b7ea8c08f53
Jaan Koppe <jaan.koppe@tigu.hk.tlu.ee>
2015-04-02 17:13:15 +0300

    muudan jaan.php faili

--- kastikesed/jaan.php
+++ kastikesed/jaan.php
@@ -3,6 +3,20 @@
 <head>
 	<title>Omaduste loetelu</title>
 	<meta charset="utf-8">
+	<style type="text/css">
+		html, body {
+			background: #eee;
+			color: #222;
+			margin: 0;
+			padding: 0;
+			width: 100%;
+		}
+
+		li:hover {
+			background: #333;
+			color: #eee;
+		}
+	</style>
 </head>
 <body>
 	<form action="naitaja.php">
